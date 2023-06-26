a555089cbe383faa8ebf4a4346b778be0e2136a7
Manuel García Criado (laptop/linux) <manuel.garcia.cr@hotmail.com>
2023-06-26 05:16:13 +0200

    Exercise 2. At work
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	modificados:     index.html
    	nuevos archivos: js/main.js
    	modificados:     sass/main.scss
    	modificados:     sass/styles.scss

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//import "../../node_modules/popper.js/dist/popper.min.js";
+
+import "../../node_modules/bootstrap/js/dist/util/index.js";
+
+import "../../node_modules/bootstrap/js/dist/base-component";
+import "../../node_modules/bootstrap/js/dist/button.js";
+import "../../node_modules/bootstrap/js/dist/collapse.js";
+import "../../node_modules/bootstrap/js/dist/tab.js";
