2084a3bb753cd3de8bd3dfc6044614ad5369be78
Manuel García Criado (laptop/linux) <manuel.garcia.cr@hotmail.com>
2023-06-28 03:08:31 +0200

    Exercise 5
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	nuevos archivos: img/icon-facebook.svg
    	nuevos archivos: img/icon-hamburger.svg
    	nuevos archivos: img/icon-twitter.svg
    	nuevos archivos: img/logo-bookmark-dark.svg
    	modificados:     index.html
    	borrados:        js/main.js
    	modificados:     sass/main.scss
    	modificados:     sass/styles.scss

--- js/main.js
+++
@@ -1,8 +0,0 @@
-//import "../../node_modules/popper.js/dist/popper.min.js";
-
-import "../../node_modules/bootstrap/js/dist/util/index.js";
-
-import "../../node_modules/bootstrap/js/dist/base-component";
-import "../../node_modules/bootstrap/js/dist/button.js";
-import "../../node_modules/bootstrap/js/dist/collapse.js";
-import "../../node_modules/bootstrap/js/dist/tab.js";
